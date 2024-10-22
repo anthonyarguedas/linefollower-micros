94957152f34b19b830e3722f2a2ffaa97d00b573
Anthony Arguedas <78665122+anthonyarguedas@users.noreply.github.com>
2024-10-21 23:17:36 -0600

    Code structure refactor

---
+++ tests/color/color.ino
@@ -0,0 +1,27 @@
+#include <Wire.h>
+#include <Adafruit_TCS34725.h>
+#include "ColorDetection.h"
+
+// Create the TCS34725 instance
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
+
+void setup() {
+  Serial.begin(115200);
+
+  if (tcs.begin()) {
+    //Serial.println("Found sensor");
+  } else {
+    Serial.println("No TCS34725 found ... check your connections");
+    while (1); // halt!
+  }
+}
+
+void loop() {
+  uint8_t colorCode;
+  getColorCode(&colorCode);
+  printColorCode(colorCode);
+  Serial.println();
+  Serial.println();
+
+  delay(500);
+}
