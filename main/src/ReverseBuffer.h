2d99ce1fe875aa57bf00eb356c6d1e58a419a20d
anthonyarguedas <78665122+anthonyarguedas@users.noreply.github.com>
2024-11-18 18:17:04 -0600

    Create ReverseBuffer.h

---
+++ main/src/ReverseBuffer.h
@@ -0,0 +1,10 @@
+#ifndef REVERSE_BUFFER_H
+#define REVERSE_BUFFER_H
+
+#define BUFFER_SIZE 350
+
+void writeBuffer(int value);
+int writeBuffer();
+void refreshBuffer();
+
+#endif // REVERSE_BUFFER_H
