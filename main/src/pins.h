823d7f2b60998747a69379363e535a8b3d0bcf75
anthonyarguedas <78665122+anthonyarguedas@users.noreply.github.com>
2024-11-20 01:37:00 -0600

    Add BW array pins

--- main/src/pins.h
+++ main/src/pins.h
@@ -15,6 +15,7 @@
 #define BIN1 3
 #define BIN2 4
 
+// Array analógico
 #define D8 23
 #define D7 22
 #define D6 21
@@ -23,5 +24,15 @@
 #define D3 16
 #define D2 15
 #define D1 14
+
+// Array digital
+#define BW1 0
+#define BW2 1
+#define BW3 2
+#define BW4 3
+#define BW5 4
+#define BW6 5
+#define BW7 6
+#define BW8 10
 
 #endif
