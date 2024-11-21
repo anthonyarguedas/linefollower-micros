dfecef73137b752a46b41db1cb30230856134288
Bernal1229 <113397076+Bernal1229@users.noreply.github.com>
2024-11-21 03:04:22 -0600

    Add QTR Wrappers

--- main/src/LineDetection.h
+++ main/src/LineDetection.h
@@ -7,20 +7,19 @@
 #include "globals.h"
 
 #define sensorCount 8
+#define NUM_SAMPLES_PER_SENSOR 4
 #define sensorCountBW 8
 
 #define OUT_OF_BOUNDS_THRESHOLD 750
 
 void initLineDetectorPins();
-unsigned int* readArray();
 unsigned int* readArrayBW();
 void printArray(unsigned int* values);
-void calculateMaxMin();
+void printMeasurements()
 void calibrateLineDetector();
 unsigned int* readArrayCalibrated();
-bool isOutOfBounds();
+bool isFork();
 bool isOutOfBoundsBW();
-bool isOutOfBoundsRead();
 int getLinePosition();
 int getLinePositionBW();
 
